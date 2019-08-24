006bbb1df0b13edda598799286b0a57f733e024e
Burak <b.karatas0@gmail.com>
2019-08-24 22:19:50 +0200

    spotify stats

---
+++ spotifystats/precache-manifest.25a2a6c8cedf9aceecff9ed3ff42833f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416fa5153520d4e8d9d109e8ea48767a",
+    "url": "/spotifystats/index.html"
+  },
+  {
+    "revision": "979a27a8ca1cd1c1b78a",
+    "url": "/spotifystats/static/css/main.e627929b.chunk.css"
+  },
+  {
+    "revision": "ed324feed0ccec0d3aa8",
+    "url": "/spotifystats/static/js/2.25925bf1.chunk.js"
+  },
+  {
+    "revision": "979a27a8ca1cd1c1b78a",
+    "url": "/spotifystats/static/js/main.007ad077.chunk.js"
+  },
+  {
+    "revision": "5065007757c37979ff40",
+    "url": "/spotifystats/static/js/runtime~main.4d944a17.js"
+  }
+]);
